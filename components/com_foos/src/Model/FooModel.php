80166098f4baaa0d62738b02a6dd431009e7954a
MATHEADS\urajkovic <uros.rajkovic@mathema.de>
2022-04-26 19:50:10 +0200

    text field from meny item showed in front

--- components/com_foos/src/Model/FooModel.php
+++ components/com_foos/src/Model/FooModel.php
@@ -3,8 +3,8 @@
 
 namespace FooNamespace\Component\Foos\Site\Model;
 
-\defined('_JEXEC') or die;
-
+defined('_JEXEC') or die;
+use Joomla\CMS\Factory;
 use Joomla\CMS\MVC\Model\BaseDatabaseModel;
 
 class FooModel extends BaseDatabaseModel
@@ -13,9 +13,18 @@ class FooModel extends BaseDatabaseModel
 
     public function getMsg()
     {
+        /*
         if (!isset($this->message)) {
             $this->message = 'Hello Foo!';
         }
+        */
+        $app = Factory::getApplication();
+        $this->message = $app->input->get('show_text', "Hi");
         return $this->message;
     }
 }
+/*
+ You can do without the check if (!isset($this->message)) in the new variant because the statement get('show_text', "Hi");
+catches the error that occurs when the parameter show_text is not set. Whenever the value show_text is not set, the second parameter "Hi" is used as default.
+
+ */
--- components/com_foos/tmpl/foo/default.php
+++ components/com_foos/tmpl/foo/default.php
@@ -2,4 +2,4 @@
 defined('_JEXEC') or die;
 ?>
     Hello Foos: <?php echo $this->msg;
-    
+
